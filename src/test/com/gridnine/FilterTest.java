1de1325621c02b43a0a449bd7b42872e7b954a03
79650 <last137954682@gmail.com>
2023-11-14 18:09:47 +0300

    flight

--- src/test/com/gridnine/FilterTest.java
+++ src/test/com/gridnine/FilterTest.java
@@ -4,60 +4,59 @@ import main.com.gridnine.model.AirTravel;
 import main.com.gridnine.model.Segment;
 import main.com.gridnine.services.Filter;
 import org.junit.jupiter.api.Test;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FilterTest {
     @Test
-    public void shouldReturnEmptyWhenDepartureDateBeforeNowFilter(){
-        Segment segment = new Segment(LocalDateTime.now().minusHours(1),LocalDateTime.now());
+    public void shouldReturnEmptyWhenDepartureDateBeforeNowFilter() {
+        Segment segment = new Segment(LocalDateTime.now().minusHours(1), LocalDateTime.now());
         List<Segment> segmentList = new ArrayList<>();
         segmentList.add(segment);
         List<AirTravel> flights = new ArrayList<>();
         flights.add(new AirTravel(segmentList));
         List<AirTravel> filteredFlights = Filter.filterNotValidDepartureDates(flights);
-        assertTrue(filteredFlights.isEmpty(),"Список перелетов должен быть пустым");
+        assertTrue(filteredFlights.isEmpty(), "Список перелетов должен быть пустым");
     }
+
     @Test
-    public void shouldReturnNotEmptyWhenDepartureDateNowOrAfterFilter(){
+    public void shouldReturnNotEmptyWhenDepartureDateNowOrAfterFilter() {
         Segment segment = new Segment(LocalDateTime.now().plusHours(1), LocalDateTime.now().plusHours(3));
         List<Segment> segmentList = new ArrayList<>();
         segmentList.add(segment);
         List<AirTravel> flights = new ArrayList<>();
         flights.add(new AirTravel(segmentList));
         List<AirTravel> filteredFlights = Filter.filterNotValidDatesDepartureAndArrival(flights);
-        assertEquals(1, filteredFlights.size(),"В списке перелетов должен быть один элемент");
+        assertEquals(1, filteredFlights.size(), "В списке перелетов должен быть один элемент");
     }
 
     @Test
-    public void shouldReturnEmptyWhenArrivalDateBeforeDepartureDateFilter(){
+    public void shouldReturnEmptyWhenArrivalDateBeforeDepartureDateFilter() {
         Segment segment = new Segment(LocalDateTime.now(), LocalDateTime.now().minusHours(1));
         List<Segment> segmentList = new ArrayList<>();
         segmentList.add(segment);
         List<AirTravel> flights = new ArrayList<>();
         flights.add(new AirTravel(segmentList));
         List<AirTravel> filteredFlights = Filter.filterNotValidDatesDepartureAndArrival(flights);
-        assertTrue(filteredFlights.isEmpty(),"Список перелетов должен быть пустым");
+        assertTrue(filteredFlights.isEmpty(), "Список перелетов должен быть пустым");
     }
 
     @Test
-    public void shouldReturnNotEmptyWhenArrivalDateAfterDepartureDateFilter(){
+    public void shouldReturnNotEmptyWhenArrivalDateAfterDepartureDateFilter() {
         Segment segment = new Segment(LocalDateTime.now(), LocalDateTime.now().plusHours(1));
         List<Segment> segmentList = new ArrayList<>();
         segmentList.add(segment);
         List<AirTravel> flights = new ArrayList<>();
         flights.add(new AirTravel(segmentList));
         List<AirTravel> filteredFlights = Filter.filterNotValidDatesDepartureAndArrival(flights);
-        assertEquals(1, filteredFlights.size(),"В списке перелетов должен быть один элемент");
+        assertEquals(1, filteredFlights.size(), "В списке перелетов должен быть один элемент");
     }
 
     @Test
-    public void shouldReturnEmptyWhenLongWaitingInterval(){
+    public void shouldReturnEmptyWhenLongWaitingInterval() {
         Integer interval = 30;
         LocalDateTime time = LocalDateTime.now();
         Segment segment1 = new Segment(time.minusHours(1), time);
@@ -68,11 +67,11 @@ public class FilterTest {
         List<AirTravel> flights = new ArrayList<>();
         flights.add(new AirTravel(segmentList));
         List<AirTravel> filteredFlights = Filter.filterFlightsWhenIntervalMoreThan(flights, interval);
-        assertTrue(filteredFlights.isEmpty(),"Список перелетов должен быть пустым");
+        assertTrue(filteredFlights.isEmpty(), "Список перелетов должен быть пустым");
     }
 
     @Test
-    public void shouldReturnEmptyWhenNormalWaitingInterval(){
+    public void shouldReturnEmptyWhenNormalWaitingInterval() {
         Integer interval = 250;
         LocalDateTime time = LocalDateTime.now();
         Segment segment1 = new Segment(time.minusHours(1), time);
@@ -83,6 +82,6 @@ public class FilterTest {
         List<AirTravel> flights = new ArrayList<>();
         flights.add(new AirTravel(segmentList));
         List<AirTravel> filteredFlights = Filter.filterFlightsWhenIntervalMoreThan(flights, interval);
-        assertEquals(1, filteredFlights.size(),"В списке должен быть один перелет");
+        assertEquals(1, filteredFlights.size(), "В списке должен быть один перелет");
     }
 }
